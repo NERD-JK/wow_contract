85b08e5b9cac025a8eb4c71bf460dd4ab0aaac9b
NERD-JK <70633242+NERD-JK@users.noreply.github.com>
2023-02-23 18:48:30 +0900

    Update haechi.sol

--- contracts/haechi.sol
+++ contracts/haechi.sol
@@ -1,17 +1,13 @@
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 pragma solidity ^0.8.0;
 
+import "contracts/parents.sol";
 
 contract Haechi is ERC20, Ownable {
     uint8 private _decimals;
-    mapping(address => bool) private _pausedUsers;
 
-    event PausedUser(address sender, address account);
-    event UnpausedUser(address sender, address account);
-    
     constructor() ERC20("Haechi", "HAC") {
         _decimals = 18;
         _mint(msg.sender, 1000000000 * 10 ** decimals());
