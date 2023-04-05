22e7ca622a76f1f670111137844d695bf5df2618
NERD-JK <70633242+NERD-JK@users.noreply.github.com>
2023-04-05 16:19:29 +0900

    Update and rename haechi.sol_flattened.sol to wow.sol_flattened.sol

--- contracts/wow.sol_flattened.sol
+++ contracts/wow.sol_flattened.sol
@@ -598,17 +598,20 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
 
 pragma solidity ^0.8.0;
 
-// File: contracts/haechi.sol
+// File: contracts/wow.sol
 
+
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 pragma solidity ^0.8.0;
 
 
-contract Haechi is ERC20, Ownable {
+contract Wow is ERC20, Ownable {
     uint8 private _decimals;
 
-    constructor() ERC20("Haechi", "HAC") {
+    constructor() ERC20("WOWMetaverse", "WOW") {
         _decimals = 18;
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+        _mint(msg.sender, 10000000000 * 10 ** decimals());
     }
 
     function decimals() public view virtual override returns (uint8) {
